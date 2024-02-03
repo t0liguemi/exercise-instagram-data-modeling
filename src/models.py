7efc7138e765c482acabcb44b752ca4e79d92fb5
Miguel <142357205+t0liguemi@users.noreply.github.com>
2024-02-03 05:22:16 +0000

    listo

--- src/models.py
+++ src/models.py
@@ -1,32 +1,66 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Enum, Boolean
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
+class User(Base):
+    __tablename__ = 'user'
+    # Here we define columns for the table user
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
-
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
-
+    public_profile = Column(Boolean)
+    active_user = Column(Boolean)
+    posts = relationship("Post",backref="user")
+    comments = relationship("Comment",backref="user")
     def to_dict(self):
-        return {}
+        return {
+            "user.id":self.id,
+            "name":self.name,
+        }
+    
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key=True)
+    caption = Column(String(250))
+    user_id = Column(ForeignKey("user.id"))
+    media = relationship("Media",backref="post")
+    user = relationship("User",backref="post")
+    comments = relationship("Comment",backref="post")
+
+
+class Media(Base):
+    __tablename__ = "media"
+    id = Column(Integer, primary_key=True)
+    type = Column(Enum("picture","video"))
+    url = Column(String(250))
+    post_id = Column(Integer,ForeignKey("post.id"))
+
+class Comment(Base):
+    __tablename__ = "comment"
+    id = Column(Integer, primary_key=True)
+    user_id = Column(ForeignKey('user.id'))
+    content = Column(String(250))
+    post_id = Column(ForeignKey('post.id'))
+
+class Follower(Base):
+    __tablename__ = "follower"
+    id = Column(Integer, primary_key=True)
+    follower_user = Column(ForeignKey('user.id'))
+    followed_user = Column(ForeignKey('user.id'))
+
+class Blocked(Base):
+    __tablename__ = "blocked"
+    id = Column(Integer, primary_key=True)
+    blocking_user = Column(ForeignKey('user.id'))
+    blocked_user = Column(ForeignKey('user.id'))
+
+
+    
 
 ## Draw from SQLAlchemy base
 try:
